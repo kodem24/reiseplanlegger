bb4c60f6eb6c91f9eefd7262714bd93480b33265
kodem24 <a@a.a>
2022-06-02 12:47:03 +0200

    lagt til farger

--- reise.py
+++ reise.py
@@ -1,17 +1,19 @@
 
 
 addr = 'https://api.entur.io/journey-planner/v3/graphql'
-
+#stasjon = 58381
+stasjon = 6298 
 query = """
 {
   stopPlace(
-    id: "NSR:StopPlace:6298"
+    id: "NSR:StopPlace:"""+ str(stasjon) +""""
   ) {
     id
     name
     estimatedCalls(
       timeRange: 72100,
-      numberOfDepartures: 5
+      numberOfDepartures: 5,
+      whiteListedModes: [metro]
     ) {
       realtime
       aimedArrivalTime
@@ -59,6 +61,15 @@ print ("-----     -----------          -------      --------" )
 
 trains = res["data"] ["stopPlace"] ["estimatedCalls"] 
 
+from xtermcolor import colorize 
+
+l1 = 0x039bd1 
+l2 = 0xf15902
+l3 = 0xa465a2
+l4 = 0x154583
+l5 = 0x40a844
+
+
 for t in trains : 
     linje = t ["serviceJourney"] ["journeyPattern"] ["line"] ["publicCode"]
     dest = t ["destinationDisplay"] ["frontText"] 
@@ -66,7 +77,21 @@ for t in trains :
     plat = t ["quay"] ["publicCode"]
     dt = datetime.datetime.fromisoformat(dep)
     dd = dt.strftime("%H:%M")
-    print (f"{linje:9} {dest:20} {dd:10}   {plat}")
+    farge = l1 
+    if linje == "1" : 
+      farge = l1 
+    elif linje == "2" :
+      farge = l2 
+    elif linje == "3" :
+      farge = l3
+    elif linje == "4" :
+      farge = l4
+    elif linje == "5" :
+      farge = l5
+    
+    linje = colorize (f"{linje:9}", rgb = farge)
+    print (f"{linje} {dest:20} {dd:10}   {plat}")
+ 
 
 
 
